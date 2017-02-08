c3b786d88ac805d1ca5bcd7969ec8fb3300eeace
MrBim <bimtech84@gmail.com>
2017-02-08 14:32:32 +0000

    ahanged the search funct to optomise run time

--- boggle.py
+++ boggle.py
@@ -18,7 +18,10 @@ def all_grid_neighbours(grid):
     neighbours = {}
     for position in grid:
         position_neighbours = neighbours_of_position(position)
-        neighbours[position] = [p for p in position_neighbours if p in grid]
+        neighbours[position] = []
+        for p in position_neighbours:
+            if p in grid:
+                neighbours[position].append(p)
     return neighbours
 
 
@@ -26,14 +29,21 @@ def path_to_word(grid, path):
     return ''.join([grid[p] for p in path])
 
 
+def is_a_real_word(word, dictionary):
+    return word in dictionary
+
+
 def search(grid, dictionary):
     neighbours = all_grid_neighbours(grid)
     paths = []
+    full_words, stems = dictionary
 
     def do_search(path):
         word = path_to_word(grid, path)
-        if word in dictionary:
+        if is_a_real_word(word, full_words):
             paths.append(path)
+        if word not in stems:
+            return
         for next_pos in neighbours[path[-1]]:
             if next_pos not in path:
                 do_search(path + [next_pos])
@@ -48,16 +58,29 @@ def search(grid, dictionary):
 
 
 def get_dictionary(dictionary_file):
+    full_words, stems = set(), set()
+
     with open(dictionary_file) as f:
-        return [w.strip().upper() for w in f]
+        for word in f:
+            word = word.strip().upper()
+            full_words.add(word)
+
+            for i in range(1, len(word)):
+                stems.add(word[:i])
+    return full_words, stems
 
 
-def main():
-    grid = make_grid(3, 3)
-    dictionary = get_dictionary('/Users/bimwilliams/PycharmProjects/boggle-machine/words.txt')
-    words = search(grid, dictionary)
+def display_words(words):
     for word in words:
         print word
     print "Found {0} words".format(len(words))
+
+
+def main():
+    grid = make_grid(5, 5)
+    dictionary = get_dictionary('/Users/bimwilliams/PycharmProjects/boggle-machine/words.txt')
+    words = search(grid, dictionary)
+    display_words(words)
+
 
 main()
